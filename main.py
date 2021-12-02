4b5e1949f1d30ded7ca7870ffa8c96be82e422e7
Rafael Tosta Santos <rafa.ecomp@gmail.com>
2021-12-01 21:21:49 -0300

    add, edit, delete

--- main.py
+++ main.py
@@ -9,14 +9,40 @@ from model.cliente import Cliente
 # acesso as funções definidas no arquivo importado
 import model.cliente_dao as funcoes_clientes
 
-# adiciona vários clientes
-for i in range(0, 5):
+# ADCIONAR CLIENTES
+for i in range(0, 15):
     novo_cliente = Cliente(i, f'Cliente-{i}', 'rua a', '75 9 8888888')
+    # CHAMADA DA FUNÇÃO DE ADICIONAR UM CLIENTE EM CLIENTE_DAO.PY
     funcoes_clientes.adicionar(novo_cliente)
 
+# EXCLUI CLIENTE
+
+funcoes_clientes.excuir(0)
+funcoes_clientes.excuir(10)
+funcoes_clientes.excuir(5)
+funcoes_clientes.excuir(7)
+funcoes_clientes.excuir(11)
+funcoes_clientes.excuir(14)
+funcoes_clientes.excuir(13)
+funcoes_clientes.excuir(12)
+
+# EDIÇÃO DE UM CLIENTE
+
+# 1 - Qual o ID do cliente que desejo editar?
+id_cliente = 4
+
+# 2 - Pegar todas as informações do cliente com ID = 4 
+# (Pegar o objeto cliente na lista de clientes que possui o ID 4)
+
+cliente_edit = funcoes_clientes.pegarCliente(id_cliente) # retorna o objeto cliente com id = 4
+
+# 3 - Fazer a edição dos campos localmente
+cliente_edit.nome = 'Kauãn Nascimento'
+cliente_edit.endereco = 'Rua Alguma coisa, n 789'
+cliente_edit.telefone = '75 9 7894-2310'
+
+# 4 - salvar o elemento na lista
+funcoes_clientes.editar(cliente_edit)
+
+# imprimir todos os clientes
 funcoes_clientes.listar_todos()
-
-id_cliente = int(input('Digite o ID do cliente que deseja excluir: '))
-funcoes_clientes.excuir(id_cliente)
-
-
--- model/cliente_dao.py
+++ model/cliente_dao.py
@@ -1,25 +1,53 @@
-# Padrão DAO (Data Access Object) 
+# Padrão DAO (Data Access Object)
 # Centraliza o acesso aos dados dos objetos cliente
 
-#lista que irá armazenar todos os clientes do sistema
+# lista que irá armazenar todos os clientes do sistema
 lista_clientes = []
 
 # Adcionar novo cliente
+
+
 def adicionar(novo_cliente):
     lista_clientes.append(novo_cliente)
 
-# Editar cliente - Dado um objeto cliente, buscar na 
-#lista através do seu ID e atualizá-lo
+# Retorna o cliente com o id informado
+
+
+def pegarCliente(id):
+    # busca na lista de clientes
+    for cliente in lista_clientes:
+        if cliente.id == id:  # verifica se o id do objeto na lista é igual ao enviado por parâmetro
+            # retorna o objeto cliente
+            return cliente  # return -> retornar
+
+    # quando não encontrar nenhum cliente com o id informado
+    return None  # None -> vazio
+
+# Editar cliente - Dado um objeto cliente, buscar na
+# lista através do seu ID e atualizá-lo
 def editar(cliente):
-    pass
-    
+    # achar a posição na lista em que o cliente a ser editado está armazenado
+    for index in range(0, len(lista_clientes)):
+        # pego o elemento com posição definido pelo index na lista
+        cliente_atual = lista_clientes[index]
+        if cliente.id == cliente_atual.id:
+            # se forem iguais significa que o cliente está armazenado
+            # na posição definida pelo index
+            # substitui (atribui) o objeto da posição informada pelo index
+            lista_clientes[index] = cliente
+
 # Excluir cliente - Dado o ID do cliente, removê-lo da lista
 def excuir(id_cliente):
-    pass
+    for index in range(0, len(lista_clientes)):
+        cliente_atual = lista_clientes[index]
+        if id_cliente == cliente_atual.id: 
+            del lista_clientes[index]
+            # parar de buscar na lista, porque o id é único
+            return # retorna algo vazio apenas para quebrar o loop do for
 
 # Listar todos os cliente
 def listar_todos():
-    # passa por todos os clientes da lista e 
+    # passa por todos os clientes da lista e
     # chama a função imprime() desses objetos
     for cliente in lista_clientes:
         cliente.imprime()
